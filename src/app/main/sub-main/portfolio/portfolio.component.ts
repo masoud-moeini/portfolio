b9d67b659cc5a97bc6eb6cf6e92dc15b8e04e335
Masoud <indi.mj@gmail.com>
2018-09-06 16:47:19 +0430

    add button effects to portfolio items

--- src/app/main/sub-main/portfolio/portfolio.component.ts
+++ src/app/main/sub-main/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-portfolio',
@@ -10,6 +11,15 @@ export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    $(document).ready(function () {
+
+        $('.portfolio__content__items__item-container').hover(function () {
+            $(this).children('.portfolio__content__items__item-container-button').animate({opacity:'1'});
+        },function () {
+          $(this).children('.portfolio__content__items__item-container-button').animate({opacity:'0'});
+        })
+    })
   }
 
 }
